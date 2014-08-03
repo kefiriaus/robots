ebf90c263018c8d7271c9d1eb40e32410882d2b6
root <root@vps69988.ovh.net>
2014-08-03 15:36:08 +0200

    mod installation

--- ruby-on-rails-install-centos.sh
+++ ruby-on-rails-install-centos.sh
@@ -12,25 +12,28 @@ sudo yum -y install libyaml-devel libffi-devel openssl-devel make
 sudo yum -y install bzip2 autoconf automake libtool bison iconv-devel
 
 #Step 2: Install RVM
-curl -L get.rvm.io | bash -s stable
+curl -sSL https://get.rvm.io | bash -s stable --rails
+#curl -L get.rvm.io | bash -s stable
 
 #Step 3: Setup RVM Environment
-source /etc/profile.d/rvm.sh
+#source /etc/profile.d/rvm.sh
 
 #Step 4: Install Ruby
-rvm install 2.1.2
+#rvm install 2.1.2
 
 #Step 5: Setup Default Ruby Version
-rvm use 2.1.2 --default
+#rvm use 2.1.2 --default
 
 #Step 6: Check Current Ruby Version.
 ruby --version
 
 fi
 
-ln -s /usr/local/bin/gem /usr/bin/gem
-ln -s /usr/local/rvm/bin/rvm /usr/bin/rvm
+source /usr/local/rvm/scripts/rvm
 
-gem install rails
+#ln -s /usr/local/bin/gem /usr/bin/gem
+#ln -s /usr/local/rvm/bin/rvm /usr/bin/rvm
+
+#gem install rails
 #gem install passenger
 #passenger-install-apache2-module
